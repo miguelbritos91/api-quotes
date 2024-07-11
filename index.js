c4cf371b9f65b6a3c05bf411d1149d982f42154b
Miguel <miguelbritos91@gmail.com>
2024-07-11 02:23:36 -0300

    expose endpoint /price to get quotes

--- index.js
+++ index.js
@@ -1,21 +1,26 @@
 import express from "express";
 import { PORT } from "./config.js";
+import { Quotes } from "./quotes.js";
 
 const app = express();
 
 app.use(express.json());
 
 app.get("/price", async (req, res) => {
-  const response = {
-    blue: { compra: 1430, venta: 1450 },
-    oficial: { compra: 897.5, venta: 937.5 },
-    bolsa: { compra: 1370.1, venta: 1373 },
-    liqui: { compra: 1387, venta: 1392.3 },
-    cripto: { compra: 1412, venta: 1490 },
-    tarjeta: { compra: 0, venta: 1500 },
-    update: "Actualizado el 11/07/24 01:46 AM",
-  };
-  res.status(200).json(response);
+  let response = { ok: false, msg: "No se encontraron datos", response: null };
+  try {
+    const data = await Quotes.get();
+    if (data) {
+      response.ok = true;
+      response.msg = "Cotizacion actualizada";
+      response.response = data;
+    }
+    res.status(200).json(response);  
+  } catch (error) {
+    response.msg = error.message;
+    res.status(500).json(response);
+  }
+  
 });
 
 app.listen(PORT, () => {
